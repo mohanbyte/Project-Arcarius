2d65667736c9a7edf3d31229181d3046c904048b
kmohan0910 <kmohan0910@gmail.com>
2021-12-23 17:33:48 +0530

    Adding Logout

--- arcarius/src/hooks/useSignup.js
+++ arcarius/src/hooks/useSignup.js
@@ -1,11 +1,12 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { projectAuth } from '../firebase/config'
-import useAuthContext from './useAuthContext'
+import { useAuthContext } from './useAuthContext'
 
 export const useSignup = () => {
+  const [isCancelled, setIsCancelled] = useState(false)
   const [error, setError] = useState(null)
   const [isPending, setIsPending] = useState(false)
-  const {dispatch }= useAuthContext()
+  const { dispatch } = useAuthContext()
 
   const signup = async (email, password, displayName) => {
     setError(null)
@@ -22,19 +23,25 @@ export const useSignup = () => {
       // add display name to user
       await res.user.updateProfile({ displayName })
 
-      //dipatch login action
+      // dispatch login action
+      dispatch({ type: 'LOGIN', payload: res.user })
 
-      dispatch({type: 'LOGIN', payload : res.user})
-
-      setIsPending(false)
-      setError(null)
+      if (!isCancelled) {
+        setIsPending(false)
+        setError(null)
+      }
     } 
     catch(err) {
-      console.log(err.message)
-      setError(err.message)
-      setIsPending(false)
+      if (!isCancelled) {
+        setError(err.message)
+        setIsPending(false)
+      }
     }
   }
+
+  useEffect(() => {
+    return () => setIsCancelled(true)
+  }, [])
 
   return { signup, error, isPending }
 }
