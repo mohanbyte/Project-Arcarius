675c4f70648c45b4e051bad64fd8409ef8cae382
kmohan0910 <kmohan0910@gmail.com>
2021-12-24 14:42:21 +0530

    Route Handling

--- arcarius/src/App.js
+++ arcarius/src/App.js
@@ -1,31 +1,36 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+import { useAuthContext } from './hooks/useAuthContext'
 
 // pages & components
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import Signup from './pages/signup/Signup'
 import Navbar from './components/Navbar'
-import { useAuthContext } from './hooks/useAuthContext'
 
 function App() {
-  const {authIsReady}= useAuthContext()
+  const { authIsReady, user } = useAuthContext()
+
   return (
     <div className="App">
-      {authIsReady &&(
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/signup">
-            <Signup />
-          </Route>
-        </Switch>
-      </BrowserRouter>)}
+      {authIsReady && (
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              {!user && <Redirect to="/login" />}
+              {user && <Home />}
+            </Route>
+            <Route path="/login">
+              {user && <Redirect to="/" />}
+              {!user && <Login />}
+            </Route>
+            <Route path="/signup">
+              {user && user.displayName && <Redirect to="/" />}
+              {!user && <Signup />}
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
